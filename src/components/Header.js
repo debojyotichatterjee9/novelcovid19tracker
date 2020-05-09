65375066acfe4aa5b39cdadb3ed77322ef9df153
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-10 00:08:28 +0530

    hotfix for the header(responsive not working)

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,28 +6,20 @@ import Container from 'components/Container';
 const Header = () => {
     return (
         <header>
-      <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#">Novel COVID19 Tracker</a>
-
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <Link to="/" className="nav-link">Home <span class="sr-only">(current)</span></Link>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+ <div>
+    <ul>
+    <li>COVID19 Tracker</li>
+      <li>
+        <Link to="/" className="nav-link">Home</Link>
       </li>
       <li class="nav-item">
-        <Link to="/page-2/"className="nav-link">🌍Global</Link>
+        <Link to="/page-2/"className="nav-link">Global</Link>
       </li>
       <li class="nav-item">
         <Link to="/page-3/" className="nav-link">India</Link>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
     </header>
