d70c570139019da1eea2cea024428b4cab0e3ffa
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-10 14:56:09 +0530

    updated the navbar and responsive property

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,44 +6,24 @@ import { Navbar, Nav, Button, NavDropdown, Form, FormControl } from 'react-boots
 const Header = () => {
     return (
         <header>
-      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
- <div>
-    <ul>
-    <li>COVID19 Tracker</li>
-      <li>
-        <Link to="/" className="nav-link">Home</Link>
-      </li>
-      <li class="nav-item">
-        <Link to="/page-2/"className="nav-link">Global</Link>
-      </li>
-      <li class="nav-item">
-        <Link to="/page-3/" className="nav-link">India</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-
-
-
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+<Navbar bg="dark" expand="lg" fixed="top" variant="dark">
+  <Navbar.Brand href="/">COVID19 Tracker</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/page-2/">Global</Nav.Link>
+      <Nav.Link href="/page-3/">India</Nav.Link>
+      <NavDropdown title="More" id="basic-nav-dropdown">
+        <NavDropdown.Item href="https://coronavirus.jhu.edu/map.html" target="_blank">John Hopkins University COVID Tracker</NavDropdown.Item>
+        <NavDropdown.Item href="https://www.covid19india.org/" target="_blank">COVID19 Tracker India</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        <NavDropdown.Item href="https://www.google.com/search?q=covid 19 latest updates" target="_blank">Latest Updates</NavDropdown.Item>
       </NavDropdown>
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
+    <Form inline action="http://google.com/search" target="_blank">
+      <FormControl name="q" type="text" placeholder="Search" className="mr-sm-2" />
+      <Button type="submit" variant="outline-warning">Search</Button>
     </Form>
   </Navbar.Collapse>
 </Navbar>
